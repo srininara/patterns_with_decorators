9b99128c021320230bafed66c2e3938b4c349853
Srinivas Narayanan <srininara@gmail.com>
2019-09-22 08:38:54 +0530

    feat(calc basic): natural number calculator - basic implementation

---
+++ app/natural_number_calc.py
@@ -0,0 +1,38 @@
+def do(operator, arg1, arg2):
+    if operator == '+':
+        return _add(arg1, arg2)
+    elif operator == '-':
+        return _subtract(arg1, arg2)
+    elif operator == '*':
+        return _multiply(arg1, arg2)
+    elif operator == '/':
+        return divide(arg1, arg2)
+    elif operator == '^':
+        return _power(arg1, arg2)
+    else:
+        raise ValueError('Calc Error - Operator not supported')
+
+def _power(arg1, arg2):
+    if arg1 < 1 or arg2 < 1:
+        raise ValueError('Calc Error - Natural numbers only supported')
+    return arg1 ** arg2
+
+def divide(arg1, arg2):
+    if arg1 < 1 or arg2 < 1:
+        raise ValueError('Calc Error - Natural numbers only supported')
+    return arg1 / arg2
+
+def _multiply(arg1, arg2):
+    if arg1 < 1 or arg2 < 1:
+        raise ValueError('Calc Error - Natural numbers only supported')
+    return arg1 * arg2
+
+def _subtract(arg1, arg2):
+    if arg1 < 1 or arg2 < 1:
+        raise ValueError('Calc Error - Natural numbers only supported')
+    return arg1 - arg2
+
+def _add(arg1, arg2):
+    if arg1 < 1 or arg2 < 1:
+        raise ValueError('Calc Error - Natural numbers only supported')
+    return arg1 + arg2
---
+++ tests/natural_number_calc_test.py
@@ -0,0 +1,45 @@
+import pytest
+
+import app.natural_number_calc as calc
+
+
+@pytest.mark.parametrize("operator, arg1, arg2, output", [
+    ('+',3,2,5),
+    ('+',4,2,6),
+    ('-',22,12,10),
+    ('-',24,4,20),
+    ('*',12,7,84),
+    ('*',11,11,121),
+    ('/',84,6,14),
+    ('/',11,11,1),
+    ('^',11,2,121),
+    ('^',4,3,64),
+])
+def test_calculator(operator, arg1, arg2, output):
+    assert output == calc.do(operator, arg1, arg2)
+
+
+def test_calculator_operator_not_supported():
+    with pytest.raises(ValueError) as ve:
+        calc.do('%', 5, 10)
+
+    assert 'Calc Error - Operator not supported' in str(ve.value)
+
+
+@pytest.mark.parametrize("operator, arg1, arg2", [
+    ('+',3,0),
+    ('+',4,-2),
+    ('-',22,0),
+    ('-',-24,4),
+    ('*',12,0),
+    ('*',11,-4),
+    ('/',84,0),
+    ('/',12,-2),
+    ('^',11,-2),
+    ('^',-4,2),
+])
+def test_calculator_non_natural_numbers_not_supported(operator, arg1, arg2):
+    with pytest.raises(ValueError) as ve:
+        calc.do(operator, arg1, arg2)
+
+    assert 'Calc Error - Natural numbers only supported' in str(ve.value)
